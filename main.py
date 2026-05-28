2187974371e9a62a8c2034a180d4074424733f76
Krishna Sai Raj Ponneboina <90061814+KRISHNASAIRAJ@users.noreply.github.com>
2026-05-28 05:51:50 +0000

    Added Langsmith configuration

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ from langchain_groq import ChatGroq
 
 load_dotenv()
 
-llm = ChatGroq(model="llama-3.3-70b-versatile")
-
 def main():
-    llm_response = llm.invoke("What are the listed REITS in India?")
-    print(llm_response.content)
+    information="""
+    Real Estate Investment Trusts (REITs) are companies that own, operate, or finance income-generating real estate. Modeled after mutual funds, they pool capital from multiple investors. This allows individuals to earn a share of rental income and capital appreciation from commercial properties without having to buy or manage physical buildings.
+    """
+
+    prompt=f"""
+    given the information {information} how should a person add certain allocation into his portfolio?
+    """
+
+    summary_prompt_template=PromptTemplate(input_variables=["information"],template=prompt)
+
+    llm = ChatGroq(temperature=0,model="llama-3.3-70b-versatile")
+
+    chain =summary_prompt_template | llm
+    response = chain.invoke(input={"information":information})
+    print(response.content)
 
 
 if __name__ == "__main__":
