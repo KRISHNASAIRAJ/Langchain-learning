69e23297c08056c1052e50291751092dc915c122
Krishna Sai Raj Ponneboina <90061814+KRISHNASAIRAJ@users.noreply.github.com>
2026-05-28 10:24:48 +0000

    Implemented tool(Tavily) using langchain

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
-from langchain_groq import ChatGroq
 
 load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_groq import ChatGroq
+from tavily import TavilyClient
 
+tavily=TavilyClient()
+
+@tool
+def search(query:str)->str:
+    """
+    Tool that searches the internet
+    Args:
+        query: The query to search
+    Returns:
+        The search result
+    """
+    print(f"Query -> {query}")
+    return tavily.search(query=query)
+
+llm = ChatGroq(temperature=0, model="meta-llama/llama-4-scout-17b-16e-instruct")
+tools=[search]
+agent=create_agent(model=llm,tools=tools)
 def main():
-    information="""
-    Real Estate Investment Trusts (REITs) are companies that own, operate, or finance income-generating real estate. Modeled after mutual funds, they pool capital from multiple investors. This allows individuals to earn a share of rental income and capital appreciation from commercial properties without having to buy or manage physical buildings.
-    """
-
-    prompt=f"""
-    given the information {information} how should a person add certain allocation into his portfolio?
-    """
-
-    summary_prompt_template=PromptTemplate(input_variables=["information"],template=prompt)
-
-    llm = ChatGroq(temperature=0,model="llama-3.3-70b-versatile")
-
-    chain =summary_prompt_template | llm
-    response = chain.invoke(input={"information":information})
-    print(response.content)
-
+    response = agent.invoke({"messages":HumanMessage(content="Search and find the latest REITS news today")})
+    print(response)
+    response2 = agent.invoke({"messages":HumanMessage(content="Search and find the latest jobs on AI Engineer in Hyderabad")})
+    print(response2)
 
 if __name__ == "__main__":
     main()
