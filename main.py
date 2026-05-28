5a6b1b617b4757cb01e7edc189795066f4e88b3b
Krishna Sai Raj Ponneboina <90061814+KRISHNASAIRAJ@users.noreply.github.com>
2026-05-28 10:28:01 +0000

    Implemented builtin library of langchain_tavily

--- main.py
+++ main.py
@@ -5,24 +5,10 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_groq import ChatGroq
-from tavily import TavilyClient
-
-tavily=TavilyClient()
-
-@tool
-def search(query:str)->str:
-    """
-    Tool that searches the internet
-    Args:
-        query: The query to search
-    Returns:
-        The search result
-    """
-    print(f"Query -> {query}")
-    return tavily.search(query=query)
+from langchain_tavily import TavilySearch
 
 llm = ChatGroq(temperature=0, model="meta-llama/llama-4-scout-17b-16e-instruct")
-tools=[search]
+tools=[TavilySearch()]
 agent=create_agent(model=llm,tools=tools)
 def main():
     response = agent.invoke({"messages":HumanMessage(content="Search and find the latest REITS news today")})
