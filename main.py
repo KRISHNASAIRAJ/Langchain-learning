0ae088a2ff00926fb57ff9e406f9dd96525ed8db
Krishna Sai Raj Ponneboina <90061814+KRISHNASAIRAJ@users.noreply.github.com>
2026-05-28 11:03:25 +0000

    Added Output formatters to the code but not used them since we are getting error

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from dotenv import load_dotenv
+from typing import List
+from pydantic import BaseModel, Field
 
 load_dotenv()
+
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_groq import ChatGroq
 from langchain_tavily import TavilySearch
 
+class Source(BaseModel):
+    """A source of information."""
+    name: str = Field(..., description="The name of the source.")
+    url: str = Field(..., description="The URL of the source.")
+
+class AgentResponse(BaseModel):
+    """The response from the agent."""
+    content: str = Field(..., description="The content of the response.")
+    sources: List[Source] = Field(..., description="The sources of the information.")
+
 llm = ChatGroq(temperature=0, model="meta-llama/llama-4-scout-17b-16e-instruct")
 tools=[TavilySearch()]
-agent=create_agent(model=llm,tools=tools)
+agent=create_agent(model=llm,tools=tools,)
+
 def main():
     response = agent.invoke({"messages":HumanMessage(content="Search and find the latest REITS news today")})
     print(response)
